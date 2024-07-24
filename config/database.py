c7ec400e13ff9431f66740b895a6c662b49e1c36
Dmytro Podolynnyi <podolinnyjdima@gmail.com>
2024-07-24 22:33:43 +0300

    add init db revision

--- config/database.py
+++ config/database.py
@@ -1,16 +1,13 @@
 from typing import AsyncGenerator
 
-from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
-from settings.settings import DATABASE_URL
-
+from config.settings.settings import DATABASE_URL
 
 Base = declarative_base()
-metadata = MetaData()
 
 engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
--- src/core/models.py
+++ src/core/models.py
@@ -11,14 +11,16 @@ class BaseModel(Base):
     __abstract__ = True
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    uuid = Column(type_=Uuid, default=uuid.uuid4, unique=True, index=True)
-    created_at = Column(type_=DateTime, default=datetime.utcnow)
-    updated_at = Column(type_=DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    is_deleted = Column(type_=Boolean, default=False)
+    uuid = Column(Uuid, default=uuid.uuid4, unique=True, index=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    is_deleted = Column(Boolean, default=False)
 
 
 class BaseNameModel(BaseModel):
-    name = Column(type_=String(length=BaseModelConstants.NAME_MAX_LENGTH), nullable=False)
+    __abstract__ = True
+
+    name = Column(String(length=BaseModelConstants.NAME_MAX_LENGTH), nullable=False)
 
 
 class Country(BaseNameModel):
@@ -28,12 +30,14 @@ class Country(BaseNameModel):
 class Area(BaseNameModel):
     __tablename__ = "area"
 
-    country_id = Column(Integer, ForeignKey(Country.c.id), nullable=False)
-    cardinal_direction_type = Column(type_=String(length=AreaConstants.CARDINAL_DIRECTION_TYPE_MAX_LENGTH), nullable=False)
+    country_id = Column(Integer, ForeignKey(Country.id), nullable=False)
+    cardinal_direction_type = Column(
+        String(length=AreaConstants.CARDINAL_DIRECTION_TYPE_MAX_LENGTH), nullable=False
+    )
 
 
 class City(BaseNameModel):
     __tablename__ = "city"
 
-    country_id = Column(Integer, ForeignKey(Country.c.id), nullable=False)
-    area_id = Column(Integer, ForeignKey(Area.c.id), nullable=False)
+    country_id = Column(Integer, ForeignKey(Country.id), nullable=False)
+    area_id = Column(Integer, ForeignKey(Area.id), nullable=False)
